32c594069650d458b939a652c94405f729697c74
Lyudmila Reshetyuk <lady.remila@yandex.ru>
2021-10-24 16:44:19 +0300

    added 1 string

--- src/index.java
+++ src/index.java
@@ -1,5 +1,6 @@
 public class index {
     public static void main(String[] args) {
-        System.out.println("What are you doing?");;
+        System.out.println("What are you doing?")
+        System.out.println("Let's talking about it!");;;
     }
 }
